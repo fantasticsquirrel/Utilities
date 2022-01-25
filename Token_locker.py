62acf87c23b2ca8839dac4eceb771ffe5c143ac0
fantasticsquirrel <95108850+fantasticsquirrel@users.noreply.github.com>
2022-01-24 20:23:00 -0600

    Update Token_locker.py

--- Token_locker.py
+++ Token_locker.py
@@ -1,56 +1,62 @@
-@export
-def stake_CSTL(locker_id: str, IN_CSTL: int=0, AR_CSTL: int=0, HI_CSTL: int=0, CA_CSTL: int=0,  CP_CSTL: int=0):
+metadata = Hash()
+data = Hash(default_value=0)
 
-#check to see if match is private. If public, don't check the list. If private, check to see if the player is on player list.
-    if data[locker_id, 'private'] == 1:
-        playerlist = data[locker_id, 'players']
-        assert ctx.caller in playerlist, 'You are not on the list of players for this match. Contact the match creator if you wish to join.'
-
-    staked_wallets = data[locker_id, 'staked_wallets']
-    cstl = importlib.import_module(cstl_contract.get())
-    UNITS_PER_CSTL = metadata['UNITS_PER_CSTL']
-    L_units = data[locker_id, 'L_units']
-
-    IN_amount = UNITS_PER_CSTL["IN"] * IN_CSTL
-    AR_amount = UNITS_PER_CSTL["AR"] * AR_CSTL
-    HI_amount = UNITS_PER_CSTL["HI"] * HI_CSTL
-    CA_amount = UNITS_PER_CSTL["CA"] * CA_CSTL
-    CP_amount = UNITS_PER_CSTL["CP"] * CP_CSTL
-
-    cstl.transfer_from(amount=cstl_amount, to=ctx.this, main_account=ctx.caller)
-
-    if ctx.caller not in staked_wallets:
-        staked_wallets.update({ctx.caller: cstl_amount})
-    else:
-        staked_wallets[ctx.caller] += cstl_amount
-
-    data[locker_id, 'staked_wallets'] = staked_wallets #adds the staker to the dict for calculating rewards for winners and losers
-    data[locker_id, 'total_cstl'] += cstl_amount #adds total CSTL to storage for calculating rewards
-
-#add input for contract name for the token to be locked
+@construct
+def seed():
+    metadata['operator'] = ctx.caller
 
 @export
+def change_metadata(key: str, new_value: str):
+    assert ctx.caller == metadata['operator'], "only operator can set metadata"
+    metadata[key] = new_value
 
-def new_locker(locker_id : str, contract: str, lock_until_year :int, lock_until_month :int, lock_until_day :int, ):
+@export
+def new_locker(locker_id : str, contract: str, lock_until_year : int, lock_until_month : int, lock_until_day : int, ):
 
     assert bool(data[locker_id, 'match_owner']) == False, "This match has already been created, please create one with a different name."
+    assert lock_until_day != None, 'Must provide end day of lock!'
+    assert lock_until_month != None, 'Must provide end month of lock!'
+    assert lock_until_year != None, 'Must provide end year of lock!'
+    assert datetime.datetime(year=lock_until_year, month=lock_until_month, day=lock_until_day) >= now, 'Lock end cannot be in the past!'
 
     data[locker_id, 'locker_owner'] = ctx.caller
-
+    data[locker_id, 'contract'] = contract
     data[locker_id, 'staked_wallets'] = {}
-
+    data[locker_id, 'total_locked'] = 0
+    data[locker_id, 'end_date'] = datetime.datetime(year=lock_until_year, month=lock_until_month, day=lock_until_day, hour=0, minute=0, microsecond=0)
 
 @export
-def add_players(locker_id : str, add1: str='', add2: str='', add3: str='', add4: str=''):
+def stake_token(locker_id: str, tokens_to_lock: float, contract: str):
 
-    assert data[locker_id, 'match_owner'] == ctx.caller, 'You are not the match creator and cannot add players to this match.'
-    assert data[locker_id, 'private'] == True, 'This is a public game and individual players cannot be added.'
+    assert contract == data[locker_id, 'contract'], f"The contract does not match the token locker contract of {data[locker_id, 'contract']}."
+    assert data[locker_id, 'end_date'] >= now, 'You can not stake in this locker since it has already ended.'
 
-    addlist = [add1, add2, add3, add4]
-    playerlist = data[locker_id, 'players']
+    staked_wallets = data[locker_id, 'staked_wallets']
+    token = importlib.import_module(contract)
+    token.transfer_from(amount=tokens_to_lock, to=ctx.this, main_account=ctx.caller)
 
-    for x in addlist:
-        playerlist.append(x)
+    if ctx.caller not in staked_wallets:
+        staked_wallets.update({ctx.caller: tokens_to_lock})
+    else:
+        staked_wallets[ctx.caller] += tokens_to_lock
 
-    data[locker_id, 'players'] = playerlist
+    data[locker_id, 'staked_wallets'] = staked_wallets #adds the staker to the dict for calculating rewards for winners and losers
+    data[locker_id, 'total_locked'] += tokens_to_lock #adds total CSTL to storage for calculating rewards
+
+@export
+def end_locker(locker_id: str):
+
+    assert now >= data[locker_id, 'end_date'], f"This token locker has not ended yet. It will end on {data[locker_id, 'end_date']}."
+
+    staked_wallets = data[locker_id, 'staked_wallets']
+    token = importlib.import_module(contract)
+
+    for key, value in dict(staked_wallets).items():
+        token.transfer(amount=value, to=key)
+
+    data[locker_id, 'locker_owner'] = None
+    data[locker_id, 'contract'] = None
+    data[locker_id, 'staked_wallets'] = {}
+    data[locker_id, 'total_locked'] = 0
+    data[locker_id, 'end_date'] = None
 
